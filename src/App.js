8570cd9e08db1e2d51986fd4cb5e9617c783db70
Maximiliano Piderit <maximiliano.piderit.a@gmail.com>
2022-09-23 03:38:18 -0300

    ahora funciona

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [color, setColor] = useState('');
+
+  function toggle(e) {
+    setColor(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='top-lights'></div>
+      <div className='body-lights'>
+        <button value='red' className={'red-light ' + ((color === 'red') ? 'active-red' : '')} onClick={toggle}></button>
+        <button value='yellow' className={'yellow-light ' + ((color === 'yellow') ? 'active-yellow' : '')} onClick={toggle}></button>
+        <button value='green' className={'green-light ' + ((color === 'green') ? 'active-green' : '')} onClick={toggle}></button>
+      </div>
     </div>
   );
 }
